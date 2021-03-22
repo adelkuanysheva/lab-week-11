04874b3a144564419860d88e8fd1efd1d3ec8553
Adel Kuanysheva <akuanysheva@my.bcit.ca>
2021-03-22 13:11:49 -0400

    Todo list and empty list initalized

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,12 @@
 // your JavaScript goes here, you can delete this comment
+
+const todoList = [ 
+
+    {todo : "clone repository for starter code", status : "complete"},
+
+    {todo : "modify app.js file to include a for loop", status : "started"}
+
+]
+
+let completedTodos = []
+
